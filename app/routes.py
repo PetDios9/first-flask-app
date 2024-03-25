e15d82fb79b795c78d101e8372ca3546d8a1089c
Peter Diosdado <diosdadopeter@gmail.com>
2024-03-25 11:13:35 -0700

    feat: implemented ability for users to logout

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect, url_for
 from app import app, db
 from app.models import User
 import sqlalchemy as sa
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 from app.forms import LoginForm
 
 
@@ -25,17 +25,22 @@ def index():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    def login():
-        if current_user.is_authenticated:
-            return redirect(url_for("index"))
-        form = LoginForm()
-        if form.validate_on_submit():
-            user = db.session.scalar(
-                sa.select(User).where(User.username == form.username.data)
-            )
-            if user is None or not user.check_password(form.username.data):
-                flash("Invalid username or password!")
-                return redirect(url_for("login"))
-            login_user(user, remember=form.remember_me.data)
-            return redirect(url_for("index"))
-        return render_template("login.html", title="Sign In", form=form)
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data)
+        )
+        if user is None or not user.check_password(form.username.data):
+            flash("Invalid username or password!")
+            return redirect(url_for("login"))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for("index"))
+    return render_template("login.html", title="Sign In", form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
