93dd82baa09e45a209cebada11944ef557d38a4c
Peter Diosdado <diosdadopeter@gmail.com>
2024-03-25 11:52:12 -0700

    feat: implemented protected pages that req login

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,15 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app, db
 from app.models import User
 import sqlalchemy as sa
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from app.forms import LoginForm
+from urllib.parse import urlsplit
 
 
 @app.route("/")
 @app.route("/index")
+@login_required
 def index():
     user = {"username": "Peter"}
     posts = [
@@ -36,7 +38,10 @@ def login():
             flash("Invalid username or password!")
             return redirect(url_for("login"))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for("index"))
+        next_page = request.args.get("next")
+        if not next_page or urlsplit(next_page).netloc != "":
+            next_page = url_for("index")
+        return redirect(next_page)
     return render_template("login.html", title="Sign In", form=form)
 
 
