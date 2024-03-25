dc87c4a149eb333c73b8d983e24875c3c064a820
Peter Diosdado <diosdadopeter@gmail.com>
2024-03-25 10:40:44 -0700

    feat: implemented login logic to login view

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
 from flask import render_template, flash, redirect, url_for
-from app import app
+from app import app, db
+from app.models import User
+import sqlalchemy as sa
+from flask_login import current_user, login_user
 from app.forms import LoginForm
 
 
@@ -22,12 +25,17 @@ def index():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash(
-            "Login requested for user {}, remember_me={}".format(
-                form.username.data, form.remember_me.data
+    def login():
+        if current_user.is_authenticated:
+            return redirect(url_for("index"))
+        form = LoginForm()
+        if form.validate_on_submit():
+            user = db.session.scalar(
+                sa.select(User).where(User.username == form.username.data)
             )
-        )
-        return redirect(url_for("index"))
-    return render_template("login.html", title="Sign In", form=form)
+            if user is None or not user.check_password(form.username.data):
+                flash("Invalid username or password!")
+                return redirect(url_for("login"))
+            login_user(user, remember=form.remember_me.data)
+            return redirect(url_for("index"))
+        return render_template("login.html", title="Sign In", form=form)
