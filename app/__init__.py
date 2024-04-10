c8d56972a589bfecf0766cd4b3f31ccde60aab38
Peter Diosdado <diosdadopeter@gmail.com>
2024-04-09 21:09:22 -0700

    fix: corrects name

--- app/__init__.py
+++ app/__init__.py
@@ -36,9 +36,7 @@ if not app.debug:
     # writes err logs to file
     if not os.path.exists("logs"):
         os.mkdir("logs")
-    file_handler = RotatingFileHandler(
-        "logs/microblog.log", maxBytes=10240, backupCount=10
-    )
+    file_handler = RotatingFileHandler("logs/blog.log", maxBytes=10240, backupCount=10)
     file_handler.setFormatter(
         logging.Formatter(
             "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
@@ -48,6 +46,6 @@ if not app.debug:
     app.logger.addHandler(file_handler)
 
     app.logger.setLevel(logging.INFO)
-    app.logger.info("Microblog startup")
+    app.logger.info("blog startup")
 
 from app import routes, models, errors
